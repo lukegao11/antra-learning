819a22c18588f6edb204b37342663f68d6200e0f
Guanqun Gao <99423162+lukegao11@users.noreply.github.com>
2023-02-22 23:53:24 -0500

    Update Program.cs

--- week2-c#basics/Assignment2/Program.cs
+++ week2-c#basics/Assignment2/Program.cs
@@ -1,4 +1,4 @@
-﻿//1. Copying an Array
+//1. Copying an Array
 using HW5;
 
 int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -19,14 +19,49 @@ foreach (int i in arr)
 
 
 
-
-
-
 //2.manage a list of elements.
-ManageItem mi = new ManageItem();
-mi.Run();
+manageitem mi = new manageitem();
+mi.run();
+
+
+//3. find prime in range
+Console.WriteLine("please input the start number of the range where you want to find prime numbers");
+int startNum = int.Parse(Console.ReadLine());
+Console.WriteLine("please input the end number of the range where you want to find prime numbers");
+int endNum = int.Parse(Console.ReadLine());
+GetPrimeNum GetPrimeNum= new GetPrimeNum();
+int[] primeArr = GetPrimeNum.FindPrimesInRange(startNum, endNum);
+
+Console.WriteLine("\nthe prime numbers are:");
+foreach (int i in primeArr)
+{
+   Console.WriteLine(i);
+}
 
 
 
 
-//
+
+
+//4. rotate and sum
+RotateSumArray RotateSumArray= new RotateSumArray();
+RotateSumArray.RotateSum();
+
+
+
+
+
+//5. find the longest sequence
+Console.Write("Enter an integer array, seperat by space: ");
+int[] arrayfor5 = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+FindLongestSequence.FindLongest(arrayfor5);
+
+
+//7. find the frequent number
+Console.Write("Enter an integer array, seperat by space: ");
+int[] arrayfor7 = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int res7 = FindFrequentNum.FindMostFrequentNumber(arrayfor7);
+Console.WriteLine("the most frequent number is {0}", res7);
+
+
+
