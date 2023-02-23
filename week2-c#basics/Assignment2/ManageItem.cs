8344c6a03699958f5514bed9f6fb47cafb193075
Guanqun Gao <99423162+lukegao11@users.noreply.github.com>
2023-02-22 22:24:41 -0500

    Add files via upload

---
+++ week2-c#basics/Assignment2/ManageItem.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5
+{
+    public class ManageItem
+    {
+        public void Run() {
+            List<string> my_list = new List<string>();
+
+            // Infinite loop until user exits
+            while (true)
+            {
+                // Ask the user for input
+                Console.Write("Enter an operation (+item, -item, or --): ");
+                string input = Console.ReadLine();
+
+                // Determine the operation to perform
+                if (input.StartsWith("+"))
+                {
+                    // Add the item to the list
+                    string item = input.Substring(1);
+                    my_list.Add(item);
+                }
+                else if (input.StartsWith("-"))
+                {
+                    // Remove the item from the list
+                    string item = input.Substring(1);
+                    my_list.Remove(item);
+                }
+                else if (input == "--")
+                {
+                    Console.WriteLine("Clearing");
+                    // Clear the list
+                    my_list.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                    continue;
+                }
+
+                // Display the current contents of the list
+                Console.WriteLine("Current list:");
+                foreach (string item in my_list)
+                {
+                    Console.WriteLine("- " + item);
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+        }
+
+
+
+    }
+}
---
+++ week2-c#basics/Assignment2/Program.cs
@@ -0,0 +1,32 @@
+﻿//1. Copying an Array
+using HW5;
+
+int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+int[] arr1 = new int[10];
+for (int i = 0; i < arr.Length; i++) {
+    arr1[i] = arr[i];
+}
+Console.WriteLine("print the original array:");    
+foreach (int i in arr)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("\nprint the second array:");
+foreach (int i in arr)
+{
+    Console.WriteLine(i);
+}
+
+
+
+
+
+
+//2.manage a list of elements.
+ManageItem mi = new ManageItem();
+mi.Run();
+
+
+
+
+//
