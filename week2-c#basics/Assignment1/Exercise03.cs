77c1679946838caf02e60f6bcae35c3b36532f03
Guanqun Gao <99423162+lukegao11@users.noreply.github.com>
2023-02-23 12:37:43 -0500

    Add files via upload

---
+++ week2-c#basics/Assignment1/Exercise03.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingTypes
+{
+    public class Exercise03
+    {
+        public void FizzBuzz() {
+
+            Console.Write("Please input a number for incrementation: ");
+            int num = int.Parse(Console.ReadLine());
+
+            foreach (int i in Enumerable.Range(0, num + 1)) {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine(
+                        "fizz"
+                    );
+                }
+                else Console.WriteLine(i);
+            }
+
+        }
+
+    }
+}
---
+++ week2-c#basics/Assignment1/PrintPyramid.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingTypes
+{
+    public static class PrintPyramid
+    {
+        public static void Pyramidprinter(int num) {
+            for (int i = 0; i < num; i++) {
+                Console.WriteLine("\n");
+                for (int j = 0; j < num - i - 1; j++) {
+                    Console.Write(" ");
+                }
+                for (int l = 0; l < 2 * i + 1; l++) { 
+                
+                    Console.Write("*");
+                }
+
+                for (int k = 0; k < num - i - 1; k++)
+                {
+                    Console.Write(" ");
+                }
+            }
+        
+        }
+
+    }
+}
