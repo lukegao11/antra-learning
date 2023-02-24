f7c26edf231cc89d96fc01329c98e72bcb08495a
Guanqun Gao <99423162+lukegao11@users.noreply.github.com>
2023-02-24 11:30:20 -0500

    Add files via upload

---
+++ week2-c#basics/Assignment3/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Assignment3
+{
+    public class Ball
+    {
+        private Color Color { get; set; }
+        private int Size { get; set; }
+        private int ThrowCounter { get; set; }
+
+        public Ball(Color color, int Size, int ThrowCounter = 0) { 
+            this.Color = color; 
+            this.Size = Size;
+            this.ThrowCounter = ThrowCounter;
+        }
+
+        public void Pop() {
+            this.Size = 0;
+        }
+        public void Throw() {
+            if (this.Size > 0) { 
+                this.ThrowCounter++;
+            }
+        }
+        public int GetThrowCount() { 
+            return this.ThrowCounter;
+        }
+
+
+
+
+
+    }
+
+}
--- week2-c#basics/Assignment3/Program.cs
+++ week2-c#basics/Assignment3/Program.cs
@@ -1,4 +1,4 @@
-using Week2Assignment3;
+﻿using Week2Assignment3;
 
 
 //Q1 reverse array
@@ -12,7 +12,8 @@ ReverseArray.PrintNumbers(numbers);
 
 //Q2.Fibonacci 
 Console.WriteLine("\nQ2");
-for (int i = 1; i < 11; i++) {
+for (int i = 1; i < 11; i++)
+{
     Console.Write(Fibonacci.nthFibonacci(i));
     Console.Write(", ");
 }
@@ -21,12 +22,41 @@ for (int i = 1; i < 11; i++) {
 
 
 
+//Q7. Throw balls
+Ball b1 = new Ball(new Color(11, 22, 33), 10);
+Ball b2 = new Ball(new Color(11, 22, 33), 10);
+Ball b3 = new Ball(new Color(11, 22, 33), 10);
+Ball b4 = new Ball(new Color(11, 22, 33), 10);
+
+b1.Throw();
+b2.Throw();
+b3.Throw();
+b4.Throw();
+
+b1.Throw();
+b2.Throw();
+b3.Throw();
+b4.Throw();
+
+Console.WriteLine($"b1 throw counts: {b1.GetThrowCount()}");
+Console.WriteLine($"b2 throw counts: {b2.GetThrowCount()}");
+Console.WriteLine($"b3 throw counts: {b3.GetThrowCount()}");
+Console.WriteLine($"b4 throw counts: {b4.GetThrowCount()}");
+
+
+b3.Pop();
+b4.Pop();
 
 
 
+b1.Throw();
+b2.Throw();
+b3.Throw();
+b4.Throw();
 
-
-
-
-
+Console.WriteLine("Post-pop:");
+Console.WriteLine($"b1 throw counts: {b1.GetThrowCount()}");
+Console.WriteLine($"b2 throw counts: {b2.GetThrowCount()}");
+Console.WriteLine($"b3 throw counts: {b3.GetThrowCount()}");
+Console.WriteLine($"b4 throw counts: {b4.GetThrowCount()}");
 
