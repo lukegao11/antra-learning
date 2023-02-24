0cc230d9c2a27b4b011ede4770fe23165090287e
Guanqun Gao <99423162+lukegao11@users.noreply.github.com>
2023-02-24 11:08:37 -0500

    Add files via upload

---
+++ week2-c#basics/Assignment3/Color.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Assignment3
+{
+    public class Color
+    {
+        public int red{ get; set; }
+        public int green { get; set; }
+        public int blue { get; set; }
+        public int alpha { get; set; }
+        public Color(int red, int green, int blue, int alpha) { 
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+        }
+        public Color(int red, int green, int blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = 225;
+        }
+
+        public int GetGrayscale() { return (red + blue + green)/3;}
+
+
+
+
+    }
+}
