ab6a381a2a6517fea5b4a63d81a7ccfd720ff9e2
Guanqun Gao <99423162+lukegao11@users.noreply.github.com>
2023-02-24 01:04:26 -0500

    Add files via upload

---
+++ week2-c#basics/Assignment3/Fibonacci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Assignment3
+{
+    public static class Fibonacci
+    {
+        public static int nthFibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            int a = 0, b = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                int c = a + b;
+                a = b;
+                b = c;
+            }
+
+            return b;
+        }
+
+
+    }
+}
