53a2947d85edd37669a0f03d6b9e415d7a6e2de4
Guanqun Gao <99423162+lukegao11@users.noreply.github.com>
2023-02-24 10:15:54 -0500

    Add files via upload

---
+++ week2-c#basics/Assignment3/Department.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Assignment3
+{
+    public class Department : IDepartmentService
+    {
+        public Instructor HeadInstructor { get; set; }
+        public decimal Budget { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<Course> OfferedCourses { get; set; }
+
+        // Constructor to initialize all properties
+        public Department(Instructor headInstructor, decimal budget, DateTime startDate, DateTime endDate, List<Course> offeredCourses)
+        {
+            HeadInstructor = headInstructor;
+            Budget = budget;
+            StartDate = startDate;
+            EndDate = endDate;
+            OfferedCourses = offeredCourses;
+        }
+
+    }
+}
