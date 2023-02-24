515eb776711a865efb85155ed610c6372e02c9df
Guanqun Gao <99423162+lukegao11@users.noreply.github.com>
2023-02-24 02:59:25 -0500

    Update Program.cs

--- week2-c#basics/Assignment3/Program.cs
+++ week2-c#basics/Assignment3/Program.cs
@@ -1,14 +1,28 @@
-﻿using Week2Assignment3;
+using Week2Assignment3;
 
 
 //Q1 reverse array
+Console.WriteLine("\nQ1");
 ReverseArray.GenerateNumbers(10);
 int[] numbers = ReverseArray.GenerateNumbers(10);
 ReverseArray.PrintNumbers(numbers);
 ReverseArray.Reverse(numbers);
 ReverseArray.PrintNumbers(numbers);
 
+
 //Q2.Fibonacci 
+Console.WriteLine("\nQ2");
+for (int i = 1; i < 11; i++) {
+    Console.Write(Fibonacci.nthFibonacci(i));
+    Console.Write(", ");
+}
+
+
+
+
+
+
+
 
 
 
