95898c9fab0aadf685d5200e394e1f42ececf666
Guanqun Gao <99423162+lukegao11@users.noreply.github.com>
2023-02-24 00:47:41 -0500

    Add files via upload

---
+++ week2-c#basics/Assignment3/Program.cs
@@ -0,0 +1,18 @@
+﻿using Week2Assignment3;
+
+
+//Q1 reverse array
+ReverseArray.GenerateNumbers(10);
+int[] numbers = ReverseArray.GenerateNumbers(10);
+ReverseArray.PrintNumbers(numbers);
+ReverseArray.Reverse(numbers);
+ReverseArray.PrintNumbers(numbers);
+
+//Q2.Fibonacci 
+
+
+
+
+
+
+
---
+++ week2-c#basics/Assignment3/ReverseArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Assignment3
+{
+    public static class ReverseArray
+    {
+
+        public static int[] GenerateNumbers(int length)
+        {
+            Random random = new Random();
+            int[] numbers = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                numbers[i] = random.Next(1, 101); // generates a random integer between 1 and 100
+            }
+
+            return numbers;
+        }
+
+        public static void Reverse(int[] numbers)
+        {
+            int left = 0;
+            int right = numbers.Length - 1;
+            while (left < right)
+            {
+                int temp = numbers[left];
+                numbers[left] = numbers[right];
+                numbers[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        public static void PrintNumbers(int[] numbers)
+        {
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+    }
+}
