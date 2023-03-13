23087cecb77f6cefbf37a70f5f376b2e70ad4e6d
Guanqun Gao <99423162+lukegao11@users.noreply.github.com>
2023-03-13 18:01:52 -0400

    Add files via upload

---
+++ week4-dotnetcore/day1recruiting/Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,13 @@
+﻿using ApplicationCore.Contracts.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class Repository : IRepository
+    {
+    }
+}
